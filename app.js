e1244053b24a7996e8b95bc7a4eebac576cf18ff
ahmetloca <locaahmet@gmail.com>
2022-06-18 19:34:10 +0300

    handlebars express

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const exphbs =require('express-handlebars');
 const app = express()
 const port = 3000
 const hostname = '127.0.0.1'
@@ -8,8 +9,13 @@ const hostname = '127.0.0.1'
 app.use(express.static('public'))
 //index.html 'de herhangi bir statik dosyaya bakmak istiyorsa önce public dosyamızın içerisine gidiyor. Sonra public dosyasının içerisindeki css dosyasına gidiyor. Ve oradaki css dosyalarını çekiyor. 
 
+app.engine('handlebars',exphbs.engine())
+app.set('view engine','handlebars')
+
+
+
 app.get('/',(req,res) => {
-  res.sendFile(path.resolve(__dirname, 'site/index.html'))
+  res.render('site2/index')
 })
 
 app.get('/about',(req,res) => {
