4eb20dd45cd5df93e6dbf0a66c5452d8a453fec9
ahmetloca <locaahmet@gmail.com>
2022-06-20 14:49:48 +0300

    Template Engine - hHandlebars Express

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ const hostname = '127.0.0.1'
 app.use(express.static('public'))
 //index.html 'de herhangi bir statik dosyaya bakmak istiyorsa önce public dosyamızın içerisine gidiyor. Sonra public dosyasının içerisindeki css dosyasına gidiyor. Ve oradaki css dosyalarını çekiyor. 
 
-app.engine('handlebars',exphbs.engine())
+app.engine('handlebars',exphbs.engine()) 
+//exphbs fonksiyonu default olarak layout klasörünün içerisine girip main.handlebars'i alıyor
 app.set('view engine','handlebars')
 
 
 
 app.get('/',(req,res) => {
-  res.render('site2/index')
+  res.render('site/index') //render express'e ait olan bir fonksiyon.
 })
 
 app.get('/about',(req,res) => {
-  res.sendFile(path.resolve(__dirname, 'site/about.html'))
+  res.render('site/about')
 })
 
 app.get('/blog',(req,res) => {
-  res.sendFile(path.resolve(__dirname, 'site/blog.html'))
+  res.render('site/blog')
 })
 
 app.listen(port, hostname, ()=> {
